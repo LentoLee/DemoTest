dc75ba4f3780e279c75ae63ed2b79fdcde0decf0
Lento <lento1943@gmail.com>
2017-09-18 18:59:08 +0800

    print name not display's contact info

--- app/src/main/java/com/example/lento/demotest/util/ContactHelper.java
+++ app/src/main/java/com/example/lento/demotest/util/ContactHelper.java
@@ -1,11 +1,12 @@
 package com.example.lento.demotest.util;
 
-import android.Manifest;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.net.Uri;
 import android.provider.CallLog;
-import android.support.v4.app.ActivityCompat;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.util.Log;
 
 import java.util.Date;
 
@@ -41,6 +42,7 @@ public class ContactHelper {
             while (managedCursor.moveToNext()) {
                 String displayName = managedCursor.getString(name);
                 String phNumber = managedCursor.getString(number);
+                checkIfNeedRefreshName(context, displayName, phNumber);
                 String callType = managedCursor.getString(type);
                 String callDate = managedCursor.getString(date);
                 Date callDayTime = new Date(Long.valueOf(callDate));
@@ -74,4 +76,33 @@ public class ContactHelper {
         return sb.toString();
 
     }
+
+    private static void checkIfNeedRefreshName(Context context, String displayName, String phNumber) {
+        if (TextUtils.isEmpty(displayName)) {
+            Log.i("xxx", "displayName is empty, so try to get name from db. number = " + phNumber);
+            getContactIDFromNumber(phNumber, context);
+        }
+    }
+
+
+    public static int getContactIDFromNumber(String contactNumber, Context context) {
+        long startTime = System.currentTimeMillis();
+
+        contactNumber = Uri.encode(contactNumber);
+        int phoneContactID = -1;
+        final Cursor contactLookupCursor = context.getContentResolver().query(Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, contactNumber),
+                new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME, ContactsContract.PhoneLookup._ID, ContactsContract.PhoneLookup.LOOKUP_KEY}, null, null, null);
+        if (contactLookupCursor != null) {
+            Log.i("xxx", "contactNumber = " + contactNumber + ", contact cursor count = " + contactLookupCursor.getCount());
+            while (contactLookupCursor.moveToNext()) {
+                phoneContactID = contactLookupCursor.getInt(contactLookupCursor.getColumnIndexOrThrow(ContactsContract.PhoneLookup._ID));
+                String phoneDisplayName = contactLookupCursor.getString(contactLookupCursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
+                Log.d("xxx", "phone display name = " + phoneDisplayName + ", contact id = " + phoneContactID);
+            }
+            contactLookupCursor.close();
+        }
+        Log.i("xxx", "cost time = " + (System.currentTimeMillis() - startTime) + " ms");
+        return phoneContactID;
+    }
+
 }
